a23ce2d4b640441fe0f2d8c725cdbbdd3f4b24cf
wphua3 <andyphuawc@gmail.com>
2025-01-17 12:26:35 +0800

    Adding google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-FR1T1MD08D");
+ReactGA.send({ hitType: "pageview", page: "/" }); // Track the initial page load
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
